b9c20f38156e4e2474f97e8ad600c73fdb45112d
vdt <sebastian.vonder.thannen@student.tuwien.ac.at>
2018-07-11 01:02:15 +0200

    first commit

---
+++ spec/detexify-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DetexifyView from '../lib/detexify-view';
+
+describe('DetexifyView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
